ceeb8bf2d40d0d973db061fd321d7138e8d7e4e8
petre <petre.ghita@appdirect.com>
2016-01-04 18:33:01 +0100

    Polished the default recipe

--- DockerHost/cookbooks/base/recipes/default.rb
+++ DockerHost/cookbooks/base/recipes/default.rb
@@ -1,11 +1,3 @@
-#ruby_block "insert_line" do
-#  block do
-#    file = Chef::Util::FileEdit.new("/etc/locale.conf")
-#    file.insert_line_if_no_match("LC_CTYPE=\"en_US.UTF-8\"", "LC_CTYPE=\"en_US.UTF-8\"")
-#    file.write_file
-#  end
-#end
-
 cookbook_file "/etc/apt/sources.list.d/docker.list" do
   source "docker.list"
     mode 0644
@@ -20,8 +12,7 @@ apt_repository 'docker' do
   key '58118E89F3A912897C070ADBF76221572C52609D'
 end
 
-#packages = ['telnet', 'htop', 'vim', 'yum-utils', 'net-tools', 'docker-engine' ]
-packages = ['linux-image-extra-$(uname -r)', 'vim', 'htop', 'docker-engine']
+packages = ['linux-image-extra-$(uname -r)', 'vim', 'telnet', 'htop', 'docker-engine']
 packages.each do |p|
   package p
 end
@@ -30,7 +21,6 @@ service 'docker' do
   supports :restart => true, :status => true, :reload => true
   action [:enable, :start]
 end
-
 
 group 'docker' do
   members ['vagrant']
